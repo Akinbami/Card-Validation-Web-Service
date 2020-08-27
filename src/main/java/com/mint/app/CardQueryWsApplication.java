812bd0ed552951e692009fe77b867884bbc62d4a
Dayo Akinbami <akinbamizacheus#gmail.com>
2020-08-27 19:54:01 +0100

    initial commit

---
+++ src/main/java/com/mint/app/CardQueryWsApplication.java
@@ -0,0 +1,13 @@
+package com.mint.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardQueryWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardQueryWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mint/app/CardQueryWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mint.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardQueryWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
